633c66e4ac74c1f72b5553e631d6a7ed100ccc69
kartemuz <kartemuz@gmail.com>
2024-09-08 17:28:26 +0300

    Added base_flow query

--- src/schedule/routers/flow.py
+++ src/schedule/routers/flow.py
@@ -6,12 +6,27 @@ from src.schedule.schemas import Flow, FlowInput
 from src.schemas import IdSchema
 from src.schedule.service import schedule_service
 from src.constants import ScheduleConstants
+from src.config import settings
 
 
 flow_router = APIRouter(
     prefix='/flow',
     tags=ScheduleConstants.TAGS
 )
+
+
+async def get_base_flow(id: Optional[int] = None) -> Flow | List[Flow]:
+    if id:
+        result: Flow = await schedule_service.flow_store.get(id)
+        if not result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    else:
+        flows: List[Flow] = await schedule_service.flow_store.get_all()
+        result: List[Flow] = []
+        for f in flows:
+            if settings.schedule.base_flow_prefix in f.name:
+                result.append(f)
+    return result
 
 
 @flow_router.get('/get')
@@ -24,6 +39,9 @@ async def get_flow(
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     else:
         result: List[Flow] = await schedule_service.flow_store.get_all()
+        for r in result:
+            if settings.schedule.base_flow_prefix in r.name:
+                result.remove(r)
     return result
 
 
