658f9f00969e1c378d5f540348c07b21aca1dbcf
kartemuz <kartemuz@gmail.com>
2024-09-25 14:59:32 +0300

    ssl

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from src.schedule.routers import schedule_router
 from src.export_.router import export_router
 from src.import_.router import import_router
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 from src.config import settings
 import ssl
 
@@ -39,6 +40,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
+    HTTPSRedirectMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=['*'],
